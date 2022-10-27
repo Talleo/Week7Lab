ff7036197e6ebe5b44b52abedee6e4a9116f9a59
Leo Talbot <leomtalbot@gmail.com>
2022-10-26 22:01:49 -0600

    Still mostly setting up the project Configured models, connectionPool, DBUtil, and contextXML

--- src/java/dataaccess/ConnectionPool.java
+++ src/java/dataaccess/ConnectionPool.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dataaccess;
 
-/**
- *
- * @author godim
- */
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.sql.*;
+
 public class ConnectionPool {
-    
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+
+    private ConnectionPool() {
+        try {
+            InitialContext ic = new InitialContext();
+            dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/notesdb");
+        } catch (NamingException e) {
+            System.out.println(e);
+        }
+    }
+
+    /* Will instantiate a new connection pool if there isn't one.
+     * If there is a pool already, then return it */
+    public static synchronized ConnectionPool getInstance() {
+        if (pool == null) {
+            pool = new ConnectionPool();
+        }
+        return pool;
+    }
+
+    // will retrieve a connection from the pool
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    // close a connection
+    public void freeConnection(Connection c) {
+        try {
+            c.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
 }
