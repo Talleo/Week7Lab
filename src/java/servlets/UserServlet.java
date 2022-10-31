eafbff08374430cdf72e5b0595409ea12a3ae4c8
Leo Talbot <leomtalbot@gmail.com>
2022-10-30 22:10:10 -0600

    Added more functionality DoGet and DoPost Added welcome file to web.xml Users.JSP coming along

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -1,27 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.User;
+import services.RoleService;
+import services.UserService;
 
-/**
- *
- * @author godim
- */
 public class UserServlet extends HttpServlet {
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -29,11 +24,54 @@ public class UserServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    } 
+            throws ServletException, IOException {
 
-    /** 
+        RoleService rs = new RoleService();
+        UserService us = new UserService();
+        String email = request.getParameter("email");
+
+        // populate the list of users
+        try {
+            List<User> users = us.getAllUsers();
+            request.setAttribute("users", users);
+            if (users.isEmpty()) {
+                request.setAttribute("message", "The list of users is empty.");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", "Something's gone wrong... very wrong.");
+        }
+
+        // get the action from request
+        String action = request.getParameter("action");
+
+        // action == edit
+        if (action != null && action.equals("edit")) {
+            try {
+                User user = us.getUser(email);
+                request.setAttribute("email", email);
+                request.setAttribute("selectedUser", user);
+                request.setAttribute("message", "User has been edited.");
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        // action == delete
+        if (action != null && action.equals("delete")) {
+            try {
+                us.deleteUser(email);
+                request.setAttribute("message", "User has been deleted.");
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -41,7 +79,48 @@ public class UserServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    }
+            throws ServletException, IOException {
 
+        RoleService rs = new RoleService();
+        UserService us = new UserService();
+        String action = request.getParameter("action");
+        String email = request.getParameter("email");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String password = request.getParameter("password");
+        int role = Integer.parseInt(request.getParameter("role"));
+
+        // action == add
+        if (action != null && action.equals("add")) {
+            try {
+                us.insertUser(email, firstName, lastName, password, role);
+                request.setAttribute("message", "User has been added.");
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+        // action == edit
+        if (action != null && action.equals("edit")) {
+            try {
+                us.updateUser(email, firstName, lastName, password, role);
+                request.setAttribute("message", "User has been edited.");
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        // set users list
+        try {
+            List<User> users = us.getAllUsers();
+            request.setAttribute("users", users);
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class
+                    .getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", "error");
+        }
+
+        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+    }
 }
--- web/WEB-INF/users.jsp
+++ web/WEB-INF/users.jsp
@@ -1,17 +1,43 @@
-<%-- 
-    Document   : users
-    Created on : 26-Oct-2022, 4:44:22 PM
-    Author     : godim
---%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Week7 Lab - User Management</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
-    </body>
+        <h1>Manage Users</h1>
+
+        <p>
+            <c:if test="${message ne null}">
+                ${message}
+            </c:if>
+        </p>
+
+        <table border="1">
+            <tr>
+                <th>Email</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Role</th>
+                <th></th>
+                <th></th>
+            </tr>
+            
+            <c:forEach var="user" items="${users}">
+                <tr>
+                    <td>${user.email}</td>
+                    <td>${user.firstName}</td>
+                    <td>${user.lastName}</td>
+                    <td>${user.role.getName()}</td>
+                    <td><a href="user?email=${user.email}&action=edit" name="edit">Edit</a></td>
+                <input type="hidden" name="action" value="delete">
+                <td><a href="user?email=${user.email}&action=delete" name="delete">Delete</a></td>
+            </tr>
+        </c:forEach>
+    </table>
+
+</body>
 </html>
